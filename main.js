db78a8d2e075b696dd5579eedb79de90855f617f
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-06-10 17:32:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+var status="";
+var video="";
 function setup(){
 canvas=createCanvas(400,300)
-canvas.center()
+canvas.position(560,300)
 }
 function draw(){
+image(video,0,0,400,300)
+}
+function preload(){
+    video=createVideo("video.mp4");
+    video.hide();
 
 }
-function preload(){}
+function cs(){
+objectDetector=ml5.objectDetector("cocossd",modeloaded)
+}
+function modeloaded(){
+    console.log("Your model has been loaded")
+    document.getElementById("Status").innerHTML="Detecting objects"
+    status=true
+    video.loop();
+    video.speed(1);
+    video.volume(0)
+}
