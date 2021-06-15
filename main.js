b83722338fa0a8423b14ef9556e5f182bd3a2ef5
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-06-15 19:47:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+object=[];
 var status="";
 var video="";
 function setup(){
@@ -6,6 +7,19 @@ canvas.position(560,300)
 }
 function draw(){
 image(video,0,0,400,300)
+if(status!=""){
+    objectDetector.detect(video,gotResult)
+document.getElementById("Status").innerHTML="Objects Detected"
+document.getElementById("noo").innerHTML=object.length;
+    for(i=0;i<object.length;i++){
+        fill(255,0,0)
+        textSize(23)
+        text(object[i].label,object[i].x,object[i].y)
+        noFill()
+        stroke("red")
+        rect(object[i].x,object[i].y,object[i].width,object[i].height)
+    }
+}
 }
 function preload(){
     video=createVideo("video.mp4");
@@ -22,4 +36,13 @@ function modeloaded(){
     video.loop();
     video.speed(1);
     video.volume(0)
+}
+function gotResult(error,result){
+if(error){
+    console.log(error)
+}
+else{
+    console.log(result)
+    object=result;
+}
 }
